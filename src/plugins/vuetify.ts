ae9bfbadb41975fa4fe9b5eaad6a84f8b5a09e9d
raulodev <raulodev@gmail.com>
2026-05-19 18:59:23 -0400

    feat: add internationalization support with vue-i18n and create English and Spanish locale files

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,15 +5,49 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+// @ts-ignore
+import "vuetify/styles";
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
+import colors from "vuetify/util/colors";
+import { createVueI18nAdapter } from "vuetify/locale/adapters/vue-i18n";
+import { createI18n, useI18n } from "vue-i18n";
+import en from "@/locales/en";
+import es from "@/locales/es";
+
+export const i18n = createI18n({
+  legacy: false,
+  locale: "es",
+  fallbackLocale: "en",
+  messages: {
+    es,
+    en,
+  },
+});
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'system',
+    defaultTheme: "light",
+    themes: {
+      light: {
+        colors: {
+          primary: colors.blue.darken1,
+        },
+      },
+    },
   },
-})
+  locale: {
+    adapter: createVueI18nAdapter({ i18n, useI18n }),
+  },
+  defaults: {
+    VBtn: {
+      color: "primary",
+      elevation: 0,
+      rounded: "lg",
+      class: "text-none",
+    },
+  },
+});
