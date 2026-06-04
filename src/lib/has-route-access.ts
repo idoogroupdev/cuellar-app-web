655c4dfe694bfac27f6901d2292efb43400c2883
raulodev <raulodev@gmail.com>
2026-06-03 21:13:41 -0400

    feat: add hasRouteAccess

---
+++ src/lib/has-route-access.ts
@@ -0,0 +1,13 @@
+import { useAppStore } from "@/stores/app";
+import { useRoute } from "vue-router";
+
+export function hasRouteAccess() {
+  const route = useRoute();
+  const { hasPermission } = useAppStore();
+
+  if (route.name == "/(private)/dashboard/users/") {
+    return hasPermission("view", "user");
+  }
+
+  return true;
+}
