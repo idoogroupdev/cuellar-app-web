9048151e2566ee29bf78e9cbcd20c1d29609cb03
raulodev <raulodev@gmail.com>
2026-06-03 21:28:20 -0400

    feat: add pinia-plugin-persistedstate to persist states

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -8,41 +8,47 @@ export type PermissionsType = "view" | "add" | "change" | "delete";
 // easy understanding in the UI I will call it section
 export type SectionType = "user";
 
-export const useAppStore = defineStore("app", () => {
-  const permissions = ref([] as PermissionNode[]);
-  const user = ref({} as UserNode);
-  // this store the email of the user that is trying to recover the password
-  const passwdRecoveryEmail = ref<null | string>();
+export const useAppStore = defineStore(
+  "app",
+  () => {
+    const permissions = ref([] as PermissionNode[]);
+    const user = ref({} as UserNode);
+    // this store the email of the user that is trying to recover the password
+    const passwdRecoveryEmail = ref<null | string>();
 
-  function setPermissions(data: PermissionNode[]) {
-    permissions.value = data;
-  }
+    function setPermissions(data: PermissionNode[]) {
+      permissions.value = data;
+    }
 
-  function setUser(data: UserNode) {
-    user.value = data;
-  }
+    function setUser(data: UserNode) {
+      user.value = data;
+    }
 
-  function setPasswdRecoveryEmail(email: string) {
-    passwdRecoveryEmail.value = email;
-  }
+    function setPasswdRecoveryEmail(email: string) {
+      passwdRecoveryEmail.value = email;
+    }
 
-  function hasPermission(permission: PermissionsType, section: SectionType) {
-    const requestPermission = `${permission}_${section}`;
+    function hasPermission(permission: PermissionsType, section: SectionType) {
+      const requestPermission = `${permission}_${section}`;
 
-    const has_permission = permissions.value.some(
-      (p) => p.codename === requestPermission,
-    );
+      const has_permission = permissions.value.some(
+        (p) => p.codename === requestPermission,
+      );
 
-    return has_permission;
-  }
+      return has_permission;
+    }
 
-  return {
-    permissions,
-    user,
-    passwdRecoveryEmail,
-    setPermissions,
-    setUser,
-    setPasswdRecoveryEmail,
-    hasPermission,
-  };
-});
+    return {
+      permissions,
+      user,
+      passwdRecoveryEmail,
+      setPermissions,
+      setUser,
+      setPasswdRecoveryEmail,
+      hasPermission,
+    };
+  },
+  {
+    persist: true,
+  },
+);
