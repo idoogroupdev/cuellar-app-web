ede8e74e81ad96c0ec6934237f19e05d753a2854
raulodev <raulodev@gmail.com>
2026-07-09 15:05:17 -0400

    refactor: simplify template structure in BranchForm component

--- src/components/branches/forms/BranchForm.vue
+++ src/components/branches/forms/BranchForm.vue
@@ -1,50 +1,48 @@
 <template>
-  <v-sheet max-width="580" class="mx-auto d-flex flex-column ga-5">
-    <v-form @submit.prevent="submit" class="pa-4 d-flex flex-column ga-3">
-      <v-text-field
-        :label="$t('forms.name')"
-        v-model="name.value.value"
-        :error-messages="name.errorMessage.value"
-        prepend-inner-icon="mdi-text"
-      />
+  <v-form @submit.prevent="submit" class="pa-4 d-flex flex-column ga-3">
+    <v-text-field
+      :label="$t('forms.name')"
+      v-model="name.value.value"
+      :error-messages="name.errorMessage.value"
+      prepend-inner-icon="mdi-text"
+    />
 
-      <v-text-field
-        :label="$t('forms.address')"
-        v-model="address.value.value"
-        :error-messages="address.errorMessage.value"
-        prepend-inner-icon="mdi-map-marker-outline"
-      />
+    <v-text-field
+      :label="$t('forms.address')"
+      v-model="address.value.value"
+      :error-messages="address.errorMessage.value"
+      prepend-inner-icon="mdi-map-marker-outline"
+    />
 
-      <v-row dense>
-        <v-col cols="12" md="6">
-          <v-switch
-            :label="$t('forms.isActive')"
-            v-model="isActive.value.value"
-            :error-messages="isActive.errorMessage.value"
-            color="primary"
-          />
-        </v-col>
-        <v-col cols="12" md="6">
-          <v-switch
-            :label="$t('forms.pickupEnabled')"
-            v-model="isPickupEnabled.value.value"
-            :error-messages="isPickupEnabled.errorMessage.value"
-            color="primary"
-          />
-        </v-col>
-      </v-row>
+    <v-row dense>
+      <v-col cols="12" md="6">
+        <v-switch
+          :label="$t('forms.isActive')"
+          v-model="isActive.value.value"
+          :error-messages="isActive.errorMessage.value"
+          color="primary"
+        />
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-switch
+          :label="$t('forms.pickupEnabled')"
+          v-model="isPickupEnabled.value.value"
+          :error-messages="isPickupEnabled.errorMessage.value"
+          color="primary"
+        />
+      </v-col>
+    </v-row>
 
-      <v-btn
-        class="mt-3"
-        type="submit"
-        block
-        append-icon="mdi-content-save"
-        :loading
-      >
-        {{ isEditing ? $t("forms.update") : $t("forms.create") }}
-      </v-btn>
-    </v-form>
-  </v-sheet>
+    <v-btn
+      class="mt-3"
+      type="submit"
+      block
+      append-icon="mdi-content-save"
+      :loading
+    >
+      {{ isEditing ? $t("forms.update") : $t("forms.create") }}
+    </v-btn>
+  </v-form>
 </template>
 <script setup lang="ts">
 import type { BranchNode } from "@/graphql/entities/branch";
